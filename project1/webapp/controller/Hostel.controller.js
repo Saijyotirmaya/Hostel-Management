72f8b3fb632b67735f2500affe450154470403cf
Sai Behera <sai.j.behera@kalpavrikshatechnologies.com>
2025-10-23 13:26:01 +0530

    Home page changes

--- project1/webapp/controller/Hostel.controller.js
+++ project1/webapp/controller/Hostel.controller.js
@@ -1,15 +1,46 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
 ],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
+   
     function (Controller) {
         "use strict";
 
         return Controller.extend("sap.ui.com.project1.controller.Hostel", {
             onInit: function () {
 
-            }
+            },
+            onNavPress: function (oEvent) {
+      const sText = oEvent.getSource().getText(); // e.g., "Home"
+      const oView = this.getView();
+
+      // Hide all sections first
+      const sections = [
+        "homeSection",
+        "roomsSection",
+        "facilitiesSection",
+        "contactSection",
+        "addDataSection"
+      ];
+      sections.forEach(id => oView.byId(id).setVisible(false));
+
+      // Show the selected one
+      switch (sText) {
+        case "Home":
+          oView.byId("homeSection").setVisible(true);
+          break;
+        case "Rooms":
+          oView.byId("roomsSection").setVisible(true);
+          break;
+        case "Facilities":
+          oView.byId("facilitiesSection").setVisible(true);
+          break;
+        case "Contact":
+          oView.byId("contactSection").setVisible(true);
+          break;
+        case "Add data":
+          oView.byId("addDataSection").setVisible(true);
+          break;
+      }
+    },
         });
     });
