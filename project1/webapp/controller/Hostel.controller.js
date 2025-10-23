d577f52718f095a53d7725926259f92d168ea574
Sai Behera <sai.j.behera@kalpavrikshatechnologies.com>
2025-10-23 18:20:45 +0530

    Room Changes

--- project1/webapp/controller/Hostel.controller.js
+++ project1/webapp/controller/Hostel.controller.js
@@ -1,46 +1,61 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-   
-    function (Controller) {
-        "use strict";
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageToast"
+], function (Controller, JSONModel, MessageToast) {
+    "use strict";
 
-        return Controller.extend("sap.ui.com.project1.controller.Hostel", {
-            onInit: function () {
+    return Controller.extend("sap.ui.com.project1.controller.Hostel", {
 
-            },
-            onNavPress: function (oEvent) {
-      const sText = oEvent.getSource().getText(); // e.g., "Home"
-      const oView = this.getView();
+        onInit: function () {
+            this._loadFilteredData();
+        },
 
-      // Hide all sections first
-      const sections = [
-        "homeSection",
-        "roomsSection",
-        "facilitiesSection",
-        "contactSection",
-        "addDataSection"
-      ];
-      sections.forEach(id => oView.byId(id).setVisible(false));
+        _loadFilteredData: function () {
+            var oView = this.getView();
+            var sUrl = "https://rest.kalpavrikshatechnologies.com/HM_Rooms";
 
-      // Show the selected one
-      switch (sText) {
-        case "Home":
-          oView.byId("homeSection").setVisible(true);
-          break;
-        case "Rooms":
-          oView.byId("roomsSection").setVisible(true);
-          break;
-        case "Facilities":
-          oView.byId("facilitiesSection").setVisible(true);
-          break;
-        case "Contact":
-          oView.byId("contactSection").setVisible(true);
-          break;
-        case "Add data":
-          oView.byId("addDataSection").setVisible(true);
-          break;
-      }
-    },
-        });
+            $.ajax({
+                url: sUrl,
+                type: "GET",
+                contentType: "application/json",
+
+                headers: {
+                    name: "$2a$12$LC.eHGIEwcbEWhpi9gEA.umh8Psgnlva2aGfFlZLuMtPFjrMDwSui",
+                    password: "$2a$12$By8zKifvRcfxTbabZJ5ssOsheOLdAxA2p6/pdaNvv1xy1aHucPm0u"
+                },
+                success: function (response) {
+                    console.log("Service call success", response);
+                    const allRooms = response?.data || [];
+                    const activeRooms = allRooms.filter((room) => room.Status === "true");
+                    const oModel = new sap.ui.model.json.JSONModel({ Rooms: activeRooms });
+                    oView.setModel(oModel, "RoomModel");
+                    sap.m.MessageToast.show("Rooms loaded successfully");
+                },
+                error: function (xhr, status, error) {
+                    console.error("AJAX Error:", status, error);
+                    sap.m.MessageToast.show("Error fetching data: " + error);
+                }
+            });
+        }
+        ,
+
+        onTabSelect: function (oEvent) {
+            var oItem = oEvent.getParameter("item");
+            const sKey = oItem.getKey();
+            this.byId("pageContainer").to(this.byId(sKey));
+
+            var page = this.byId(sKey);
+            if (page && page.scrollTo) page.scrollTo(0, 0);
+        },
+
+        onpressBookrooms: function () {
+            var oTabHeader = this.byId("mainTabHeader");
+            oTabHeader.setSelectedKey("idRooms");
+            this.byId("pageContainer").to(this.byId("idRooms"));
+
+            var page = this.byId("idRooms");
+            if (page && page.scrollTo) page.scrollTo(0, 0);
+        }
     });
+});
